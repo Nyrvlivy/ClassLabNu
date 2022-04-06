40f98460362709d5aee38981ad1a958d114036ad
Nyrlivy <nyrvlivy@gmail.com>
2022-04-06 11:21:19 -0300

    private vs public

--- ClassLabNu/Usuario.cs
+++ ClassLabNu/Usuario.cs
@@ -7,7 +7,28 @@ using System.Threading.Tasks;
 namespace ClassLabNu
 {
     // Documentação de classes de projeto. - XML Docs
-    internal class Usuario
+    public class Usuario
     {
+        // atributos
+        private int id;
+        private string nome;
+        private string email;
+        private string password;
+        private bool ativo;
+
+        // propriedades
+        public int Id { get { return id; } set { id = value; } }
+        public string Nome { get { return nome; } set { nome = value; } } 
+        public string Email { get { return email; } set { email = value; } }
+        public string Password { get 
+            { 
+                return password; 
+            } set { password = value; } }
+        public bool Ativo { get { return ativo; } set { ativo = value; } }
+
+        // métodos construtores
+
+        // métodos da classe 
+
     }
 }
