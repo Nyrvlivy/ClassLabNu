de6f7ec4833acdec6896a041948aff79ef3382f8
Nyrlivy <nyrvlivy@gmail.com>
2022-04-06 10:55:50 -0300

    Adicionar arquivos de projeto.

---
+++ ClassLabNu/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLabNu
+{
+    // Documentação de classes de projeto. - XML Docs
+    internal class Usuario
+    {
+    }
+}
