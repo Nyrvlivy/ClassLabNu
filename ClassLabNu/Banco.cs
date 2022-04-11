ccf97f654825af6593a951f42e7ee5bbb0fdf854
Nyrvlivy <nyrvlivy@gmail.com>
2022-04-11 11:55:33 -0300

    Criando Banco.cs | Aula: 11/04

---
+++ ClassLabNu/Banco.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ClassLabNu
+{
+    public class Banco
+    {
+        public static MySqlCommand Abrir() 
+        { 
+            MySqlCommand cmd = new MySqlCommand();
+
+            string strcon = @"";       //strcon = string de conexão
+            MySqlConnection cn = new MySqlConnection(strcon);
+            cn.Open();
+            cmd.Connection = cn;
+
+            return cmd;
+        }
+
+    }
+
+}
