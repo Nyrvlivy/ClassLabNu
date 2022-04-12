3ebb9327efdc5747de35099d0ca49f88b73aa471
Nyrvlivy <nyrvlivy@gmail.com>
2022-04-12 10:15:22 -0300

    StrConexao | Aula: 12.04

--- ClassLabNu/Banco.cs
+++ ClassLabNu/Banco.cs
@@ -9,12 +9,23 @@ namespace ClassLabNu
 {
     public class Banco
     {
+        // Propriedade de Conexão | string
+        public static string StrConexao { get; set; }
+
+        // Método Abrir Conexao
         public static MySqlCommand Abrir() 
         { 
             MySqlCommand cmd = new MySqlCommand();
 
-            string strcon = @"";       //strcon = string de conexão
-            MySqlConnection cn = new MySqlConnection(strcon);
+            //(Conexão do Host do senac)
+                // StrConexao = @"server=127.0.0.1;database=comercialdb0191;user id=root;password=;port=3306";     
+            //(Conexão pelo nome do Host do computador do senac)
+                // StrConexao = @"ITQ0626037W10-1";      
+            //(Conexão do Host do domínio)
+                // StrConexao = @"senac.com etc etc";    
+            // Máquina local
+                 StrConexao = @"server=127.0.0.1;database=comercialdb0191;user id=root;password=;port=3306";       
+            MySqlConnection cn = new MySqlConnection(StrConexao);
             cn.Open();
             cmd.Connection = cn;
 
--- ComercialSys91/Form1.cs
+++ ComercialSys91/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient; // não precisa, só usar como Var
+using ClassLabNu;
 
 namespace ComercialSys91
 {
@@ -15,6 +17,22 @@ namespace ComercialSys91
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var cmd = Banco.Abrir(); // F9 - mostra opções + F5 -> gera break point
+            if (cmd.Connection.State == ConnectionState.Open) 
+            {
+            Usuario usuario = new Usuario("Zé", "ze@ti.com","123456",new Nivel("Caixa","cx"));
+            }
+
+            Cliente cliente = new Cliente();
+
+            //MessageBox.Show($"Olá, {usuario.Nome}!");
+            //Text = "Sistema Comercial TI91 | " + usuario.Nome + " -- " + usuario.Nivel.Nome;
+            // OBS.: Se colocar um nível específico, você pode determinar que só um determinado nível pode usar o formulário
+            //MessageBox.Show("Olá, TI91!");
         }
     }
 }
